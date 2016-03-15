8833de699e387a4b219df84eb16eb15f6d74be01
jarne <jarne@users.noreply.github.com>
2016-03-15 20:00:40 +0100

    Anti-Caps, not the same message, new config, time between messages not working

--- src/jjmc/BadWordBlocker/BadWordBlocker.php
+++ src/jjmc/BadWordBlocker/BadWordBlocker.php
@@ -18,16 +18,41 @@ class BadWordBlocker extends PluginBase implements Listener {
 
         $this->list = $this->getConfig()->get("badwords");
         $this->list = explode(',', $this->list);
+        $this->waitingtime = $this->getConfig()->get("waitingtime");
+
         $this->blockmessage = $this->getConfig()->get("blockmessage");
+        $this->lastwritten = $this->getConfig()->get("lastwritten");
+        $this->timewritten = $this->getConfig()->get("timewritten");
+        $this->caps = $this->getConfig()->get("caps");
     }
     
     public function onPlayerChat(PlayerChatEvent $event) {
         $message = $event->getMessage();
         $player = $event->getPlayer();
         
-        if ($this->contains($message, $this->list)) {
+        if($this->contains($message, $this->list)) {
             $player->sendMessage($this->blockmessage);
             $event->setCancelled(true);
+        } elseif (isset($player->lastwritten)) {
+            if($player->lastwritten == $message) {
+                $player->sendMessage($this->lastwritten);
+                $event->setCancelled(true);
+            }
+        } elseif(isset($player->timewritten)) {
+            if($player->timewritten > new \DateTime()) {
+                $player->sendMessage($this->timewritten);
+                $event->setCancelled(true);
+            }
+        } elseif(ctype_upper($message)) {
+            $player->sendMessage($this->caps);
+            $event->setCancelled(true);
+        }
+
+        if(!$event->isCancelled()) {
+            //$player->timewritten = date_add(new \DateTime(), new \DateInterval("PT".$this->waitingtime."S"));
+            $player->timewritten = new \DateTime();
+            $player->timewritten = $player->timewritten->add(new \DateInterval("PT".$this->waitingtime."S"));
+            $player->lastwritten = $message;
         }
     }
 
